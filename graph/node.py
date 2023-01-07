2fd80f9ff6605346d61628d665f4a012e8b6caab
dreivmeister <felix.brandt@fh-bielefeld.de>
2023-01-07 02:36:26 +0100

    Add GraphNode

---
+++ graph/node.py
@@ -0,0 +1,35 @@
+class NodeInterface:
+    pass
+
+
+class GraphNode(NodeInterface):
+    def __init__(self, id, neighbors, val=None, weighted=False):
+        self.id = id
+        # can be None
+        self.val = val
+        # contains ids of neighbor nodes (unweighted graph)
+        # contains (id,weight) tuple of neighbor nodes (weighted graph)
+        self.neighbors = neighbors
+        self.weighted = weighted
+    
+    def get_neighbors(self):
+        return self.neighbors
+    
+    def get_id(self):
+        return self.id
+    
+    def get_val(self):
+        # can return None
+        return self.val
+    
+    def print_data(self):
+        print(f"id: {self.id} neighs: {self.neighbors}")
+        
+
+class Graph:
+    def __init__(self, graph_nodes, val=None, weighted=False):
+        self.graph_nodes = graph_nodes
+        self.val = val
+        self.weighted = weighted
+        
+    
---
+++ graph/utils.py
@@ -0,0 +1,37 @@
+from node import GraphNode
+from ast import literal_eval
+
+
+def load_graph(path):
+    weighted = False
+    vals = False
+    graph_nodes = []
+    with open(path) as file:
+        for i,line in enumerate(file):
+            # remove comments
+            line = line.split('#')[0].rstrip()
+            if i == 0:
+                weighted = line
+                continue
+            if i == 1:
+                vals = line
+                continue
+            if line.strip() == '':
+                continue
+            # handle all 4 cases
+            if weighted == 'False' and vals == 'False':
+                id, n = line.split(sep=' ')
+                neighbors = [int(i) for i in n.split(sep=',')]
+                graph_nodes.append(GraphNode(id=int(id),neighbors=neighbors))
+            if weighted == 'True' and vals == 'False':
+                id, n = line.split(sep=' ')
+                neighbors = [literal_eval(i) for i in n.split(sep=';')]
+                graph_nodes.append(GraphNode(id=int(id),neighbors=neighbors))
+                
+    # for i in range(len(graph_nodes)):
+    #     graph_nodes[i].print_data()
+    
+    return graph_nodes
+                
+            
+load_graph('graphconfig.txt')
