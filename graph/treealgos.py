ac605c20b4f8bcb91c4733d05e9f9e28f2090161
dreivmeister <felix.brandt@fh-bielefeld.de>
2023-01-09 22:36:04 +0100

    maxpathsum

--- graph/treealgos.py
+++ graph/treealgos.py
@@ -31,27 +31,57 @@ def eulerian_tour(root, tour):
     
     eulerian_tour(root.right_child, tour)
     tour.append(root.val)
-
-def lowest_common_ancestor(root, v, w):
-    pass
+    
+def max_path_sum(root):
+    max_path = -float('inf')
+    
+    def trav(node):
+        nonlocal max_path
+        
+        if not node:
+            return 0
+        
+        left = max(trav(node.left_child),0)
+        right = max(trav(node.right_child),0)
+        
+        max_path = max(max_path,left+right+node.val)
+        
+        return max(left+node.val,right+node.val)
+    
+    trav(root)
+    return max_path
+    
 
 if __name__=="__main__":
-    nc = BinaryTreeNode(6)
-    ne = BinaryTreeNode(7)
-    nh = BinaryTreeNode(8)
-    na = BinaryTreeNode(3)
-    nd = BinaryTreeNode(4,nc,ne)
-    nb = BinaryTreeNode(1,na,nd)
-    ni = BinaryTreeNode(5,nh)
-    ng = BinaryTreeNode(2,None,ni)
-    nf = BinaryTreeNode(0,nb,ng)
+    # nc = BinaryTreeNode(6)
+    # ne = BinaryTreeNode(7)
+    # nh = BinaryTreeNode(8)
+    # na = BinaryTreeNode(3)
+    # nd = BinaryTreeNode(4,nc,ne)
+    # nb = BinaryTreeNode(1,na,nd)
+    # ni = BinaryTreeNode(5,nh)
+    # ng = BinaryTreeNode(2,None,ni)
+    # nf = BinaryTreeNode(0,nb,ng)
+    
+    
+    root = BinaryTreeNode(10)
+    root.left_child = BinaryTreeNode(2)
+    root.right_child = BinaryTreeNode(10)
+    root.left_child.left_child = BinaryTreeNode(20)
+    root.left_child.right_child = BinaryTreeNode(1)
+    root.right_child.right_child = BinaryTreeNode(-25)
+    root.right_child.right_child.left_child = BinaryTreeNode(3)
+    root.right_child.right_child.right_child = BinaryTreeNode(4)
+    
     
     #nodelist = []
     #postorder(nf,nodelist)
     #print(nodelist) #F,B,A,D,C,E,G,I,H
     
-    tour = []
-    eulerian_tour(nf,tour)
-    print(tour) #0,1,3,1,4,6,4,7,4,1,0,2,5,8,5,2,0
+    # tour = []
+    # eulerian_tour(nf,tour)
+    # print(tour) #0,1,3,1,4,6,4,7,4,1,0,2,5,8,5,2,0
+    
+    print(max_path_sum(root))
     
     
