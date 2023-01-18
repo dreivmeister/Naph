7d1505d9025f922940950260bb197527a4cfa101
dreivmeister <felix.brandt@fh-bielefeld.de>
2023-01-18 15:09:45 +0100

    Add some more finitediff

--- ode/finite_diff_pde.py
+++ ode/finite_diff_pde.py
@@ -15,99 +15,129 @@ delta_t = (delta_x ** 2)/(4 * alpha)
 gamma = (alpha * delta_t) / (delta_x ** 2)
 
 # Boundary conditions
-boundary_conditions = [100.0,50.0,100.0,50.0] # top,left,bottom,right
+boundary_conditions = ['dirichlet',100.0,50.0,100.0,50.0] # top,left,bottom,right
 
 # Initial condition everywhere inside the grid
-initial_condition = 50.0
+initial_condition = ['fixed',50.0]
 
 
-def initialize_u(max_iter_time, 
-                 type_boundary,  
-                 type_initial,
-                 boundary_cond = None,
-                 initial_cond = None, 
-                 ni=plate_length, 
-                 nj=plate_width):
+def initialize_u_2d(max_iter_time,
+                    boundary_cond = None,
+                    initial_cond = None, 
+                    ni=plate_length,
+                    nj=plate_width):
     
-    if type_initial == 'fixed':
+    if initial_cond[0] == 'fixed':
         # Initialize solution: the grid of u(k, i, j)
-        u = np.full((max_iter_time, ni, nj), initial_cond)
-    elif type_initial == 'normal': # normal distribution
-        u = np.random.normal(loc=0.0, scale=1.0, size=(max_iter_time, ni, nj))
+        u = np.full((max_iter_time, ni, nj), initial_cond[1])
+    elif initial_cond[0] == 'normal': # normal distribution
+        u = np.random.normal(loc=initial_cond[1], scale=initial_cond[2], size=(max_iter_time, ni, nj))
 
-    if type_boundary == 'dirichlet':
+    if boundary_cond[0] == 'dirichlet':
         assert boundary_cond is not None
         # Set the boundary conditions
-        u[:, 0, :] = boundary_cond[0] #top
-        u[:, :, 0] = boundary_cond[1] #left
-        u[:, -1, :] = boundary_cond[2] #bottom
-        u[:, :, -1] = boundary_cond[3] #right
+        u[:, 0, :] = boundary_cond[1] #top
+        u[:, :, 0] = boundary_cond[2] #left
+        u[:, -1, :] = boundary_cond[3] #bottom
+        u[:, :, -1] = boundary_cond[4] #right
+    if boundary_cond[0] == 'neumann':
+        raise NotImplementedError('neumann boundary not availaible')
         
     return u
 
 
+def initialize_u_1d(max_iter_time,
+                    length_n,
+                    boundary_cond = None, 
+                    initial_cond = None):
+    
+    if initial_cond[0] == 'fixed':
+        u = np.full((max_iter_time,length_n), initial_cond[1])
+    elif initial_cond[0] == 'normal':
+        u = np.random.normal(loc=initial_cond[1], scale=initial_cond[2], size=(max_iter_time, length_n))
+    
+    if boundary_cond[0] == 'dirichlet':
+        assert boundary_cond is not None
+        # Set the boundary conditions
+        u[:, 0] = boundary_cond[1] #left
+        u[:, -1] = boundary_cond[2] #right
+    elif boundary_cond[0] == 'neumann':
+        raise NotImplementedError('neumann boundary not availaible')
+    
+    return u
+
+
 # calc 2d pde
-def calculate_u(u):
+def calculate_u_2d(u):
     nk, ni, nj = u.shape
-    for k in range(0, nk-1):
+    for k in range(nk-1):
         A = u[k, 2:  , 1:-1] #i-1,j
         B = u[k,  :-2, 1:-1] #i+1,j
         C = u[k, 1:-1, 2:  ] #i,j-1
         D = u[k, 1:-1,  :-2] #i,j+1
         E = u[k, 1:-1, 1:-1] #i,j
         F = u[k-1,1:-1,1:-1] #k-1,i,j
-        #result = gamma * (A+B+C+D-4*E) + 2*E - F #2d wave equation
-        result2 = E*(1*(D-E)+1) #inviscid burger equation
+        result = gamma * (A+B+C+D-4*E) + 2*E - F #2d wave equation
         # set the newly computed heatmap at time k+1
-        u[k+1, 1:-1, 1:-1] = result2
+        u[k+1, 1:-1, 1:-1] = result
     return u
 
-
-
-def plot_u_2d(u_k, k):
-    # Clear the current plot figure
-    plt.clf()
-
-    plt.title(f"Displacement at t = {k*delta_t:.3f} unit time")
-    plt.xlabel("x")
-    plt.ylabel("y")
-
-    # This is to plot u_k (u at time-step k)
-    plt.pcolormesh(u_k, cmap=plt.cm.jet, vmin=0, vmax=100)
-    plt.colorbar()
-
-    return plt
+def calculate_u_1d(u):
+    nk, nl = u.shape
+    for k in range(nk-1):
+        D = u[k, :-2]
+        E = u[k,1:-1]
+        result = E*(-1*(D-E)+1) #inviscid burger equation
+        u[k+1,1:-1] = result
+    return u
 
 if __name__=="__main__":
-    plot = '2D'
-    u_init = initialize_u(max_iter_time=max_iter_time, type_boundary='dirichlet', type_initial='normal', boundary_cond=boundary_conditions, ni=plate_length, nj=plate_width)
-    u_result = calculate_u(u_init)
+    plot = '1D'
+    u_init = initialize_u_1d(max_iter_time=max_iter_time, length_n=plate_length, boundary_cond=boundary_conditions, initial_cond=initial_condition)
+    u_result = calculate_u_1d(u_init)
+    
+    
+    if plot == '1D':
+        fig = plt.figure()
+        
+        def animate(k):
+            plt.clf()
+            plt.title('blalal')
+            plt.plot(u_result[k])
+            return plt
+        
+        anim = animation.FuncAnimation(fig, animate, frames=max_iter_time, repeat=False)
+        anim.save("pde_1d_solve.gif")
     
     
     if plot == '2D':
-        # Do the calculation here
-        def animate(k):
-            plot_u_2d(u_result[k], k)
-
-        
         fig = plt.figure()
-        anim = animation.FuncAnimation(fig, animate, interval=1, frames=max_iter_time, repeat=False)
+        
+        def animate(k):
+            # Clear the current plot figure
+            plt.clf()
+            plt.title(f"Displacement at t = {k*delta_t:.3f} unit time")
+            plt.xlabel("x")
+            plt.ylabel("y")
+            # This is to plot u_k (u at time-step k)
+            plt.pcolormesh(u_result[k], cmap=plt.cm.jet, vmin=0, vmax=100)
+            plt.colorbar()
+            return plt
+        
+        anim = animation.FuncAnimation(fig, animate, frames=max_iter_time, repeat=False)
         anim.save("pde_2d_solve.gif")
-        print("Done")
+        
     elif plot == '3D':
         X = np.arange(0, plate_length, 1)
         Y = np.arange(0, plate_width, 1)
         X, Y = np.meshgrid(X, Y)
-        
         fig = plt.figure()
         ax = fig.add_subplot(111,projection='3d')
-        ax.set_zlim3d(0,200)
+        
         def animate(n):
             ax.cla()
             ax.plot_surface(X,Y,u_result[n,:,:])
             return fig,
         
-        anim = animation.FuncAnimation(fig=fig,func=animate,frames=max_iter_time,repeat=False)
+        anim = animation.FuncAnimation(fig, animate, frames=max_iter_time, repeat=False)
         anim.save("pde_3d_solve.gif")
-        print("Done")
-            
